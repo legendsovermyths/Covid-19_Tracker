7b28d599e4075726dd194777c20b216d4453ae86
Mask Wolf <maskwolf2000@gmail.com>
2020-07-26 18:51:18 +0530

    add layout

--- src/App.js
+++ src/App.js
@@ -1,28 +1,59 @@
 import React,{useState,useEffect}from 'react';
 import logo from './logo.svg';
+import Map from './Map'
 import './App.css';
+import InfoBox from "./InfoBox.js"
 import {FormControl,
         MenuItem,
         Select} from '@material-ui/core';
 
 function App() {
-  const [countries, setCountries]=useState(["USA","UK","India"]);
+  const [countries, setCountries]=useState([]);
+  const [country,setCountry]=useState("Worldwide")
   useEffect(()=>{
-
-  },[countries])
+    const getCountriesData= async() =>{
+      await fetch("https://disease.sh/v3/covid-19/countries")
+      .then((response)=>response.json())
+      .then((data)=>{
+        const countries=data.map((country)=>(
+          {
+            name:country.country,
+            value:country.countryInfo.iso2
+          }));
+          setCountries(countries);
+      })
+    };
+    getCountriesData();
+  },[])
+  const onCountryChange=async (event)=>{
+    const countryCode=event.target.value;
+    setCountry(countryCode)
+  }
   return (
     <div className="App">
-      <div className="app_header">
-      <h1>COVID-19 TRACKER</h1>
-      <FormControl className="app_dropdown">
-      <Select
-        variant="outlined"
-        value="abc"
-        >
-            {countries.map((country)=><MenuItem value={country}>{country}</MenuItem>)}
-      </Select>
-      </FormControl>
-      </div>
+     <div className="app_left">
+     <div className="app_header">
+     <h1>COVID-19 TRACKER</h1>
+     <FormControl className="app_dropdown">
+     <Select
+       onChange={onCountryChange}
+       variant="outlined"
+       value={country}>
+           <MenuItem value="Worldwide">Worldwide</MenuItem>
+           {countries.map((country)=><MenuItem value={country.value}>{country.name}</MenuItem>)}
+     </Select>
+     </FormControl>
+     </div>
+     <div className="app_stats">
+       <InfoBox title="Coronavirus cases" cases={123}  total={2000}/>
+       <InfoBox title="Recovered" cases={123} total={4000}/>
+       <InfoBox title="Deaths" cases={123} total={5000}/>
+     </div>
+     <div className="map_stats">
+       <Map/>
+     </div>
+    </div>
+
     </div>
   );
 }
---
+++ src/Map.js
@@ -0,0 +1,9 @@
+import React from "react"
+function Map(){
+  return(
+    <div className="map">
+    <h1>I am a map</h1>
+    </div>
+  )
+}
+export default Map
